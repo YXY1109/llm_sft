4eb6bce83a06b3f7b2c22d944f7d15da808398db
yangxy <1049598673@qq.com>
2025-08-10 12:19:32 +0800

    fix：下载两个模型

--- chapter_5/1_download_model.py
+++ chapter_5/1_download_model.py
@@ -2,6 +2,6 @@ from modelscope import snapshot_download
 
 dp_model_dir = snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Qwen-7B', cache_dir="./models")
 print(f"deepseek模型目录：{dp_model_dir}")
-# qwen_model_dir = snapshot_download('Qwen/Qwen3-8B', cache_dir="./models")
-# print(f"qwen3模型目录：{qwen_model_dir}")
+qwen_model_dir = snapshot_download('Qwen/Qwen3-8B', cache_dir="./models")
+print(f"qwen3模型目录：{qwen_model_dir}")
 print("下载完成")
